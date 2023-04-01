78d3fe5fe847ea4673f6bce32bb63d6c400366c1
Oleg Chepelev <yorkblansh@yandex.com>
2023-04-01 13:14:08 +0300

    add commander, add test option

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,10 @@
 import React from 'react'
 import { render } from 'ink'
 import { App } from './ui.js'
+import { Command } from 'commander'
+
+const program = new Command()
+program.option('--debug_logs')
+program.parse()
 
 render(<App />)
