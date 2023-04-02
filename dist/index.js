edd22228b52f8ab9bcbe9f854ab5f26bac28dc2a
Oleg Chepelev <yorkblansh@yandex.com>
2023-04-02 14:30:28 +0300

    add commander, add debug_log option (#2)
    * add commander, add test option
    
    * 0.0.4
    
    * add comments
    
    * 0.0.5-0
    
    * add one more test comment
    
    * 0.0.6-0
    
    * clean test comments
    
    * 0.0.6
    
    * test com for npm tag
    
    * 0.0.7
    
    * remove unused comment

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,8 @@
 import React from 'react';
 import { render } from 'ink';
 import { App } from './ui.js';
+import { Command } from 'commander';
+const program = new Command();
+program.option('--debug_logs');
+program.parse();
 render(React.createElement(App, null));
