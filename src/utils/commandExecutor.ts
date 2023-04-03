83e98ad2ba770d6cf69ac566645a2a9b3c57d8d8
Oleg Chepelev <yorkblansh@yandex.com>
2023-04-03 14:57:16 +0300

    commandExecutor: add npm script handler

--- src/utils/commandExecutor.ts
+++ src/utils/commandExecutor.ts
@@ -4,6 +4,8 @@ import shelljs from 'shelljs'
 import { dockerComposeHandler } from './StdHandlers/dockerComposeHandler.js'
 import { StdHandler } from '../interfaces/StdHandler.interface.js'
 import { logger } from './logger.js'
+import { defaultHandler } from './StdHandlers/defaultHandler.js'
+import { npmScriptHandler } from './StdHandlers/npmScriptHandler.js'
 
 export const commandExecutor = (
 	{ cmd, setup }: Command,
@@ -13,10 +15,13 @@ export const commandExecutor = (
 	const childProcess = shelljs.exec(cmd, { async: true, silent: true })
 	const handlersMap: { [everyName in Setup]: StdHandler } = {
 		docker_compose: dockerComposeHandler,
-		default: () => {},
+		default: defaultHandler,
+		npm_script: npmScriptHandler,
 	}
 
-	setup
-		? handlersMap[setup](childProcess, callback, logger)
-		: handlersMap['default'](childProcess, callback, logger)
+	if (setup) {
+		handlersMap[setup](childProcess, callback, logger)
+	} else {
+		handlersMap['default'](childProcess, callback, logger)
+	}
 }
